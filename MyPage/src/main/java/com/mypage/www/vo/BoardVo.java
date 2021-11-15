6803fd36f6648b1e1f6d5a74999d998a2d3fe30b
seulgi9834 <seulgi9834@naver.com>
2021-11-15 17:51:29 +0900

    기본적인 디자인 진행

---
+++ MyPage/src/main/java/com/mypage/www/vo/BoardVo.java
@@ -0,0 +1,66 @@
+package com.mypage.www.vo;
+
+import java.util.Date;
+
+public class BoardVo {
+
+	private Integer boardNo; // 게시물 번호
+	private String boardTitle; // 게시물 제목
+	private String boardWriter; // 게시물 저자
+	private Date regDt; // 작성 날짜
+	private Integer boardViews; // 게시물 조회수
+	private String boardContent; // 게시물 내용
+	private String fileName; // 게시물 첨부파일명
+
+	public Integer getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(Integer boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+	public String getBoardWriter() {
+		return boardWriter;
+	}
+	public void setBoardWriter(String boardWriter) {
+		this.boardWriter = boardWriter;
+	}
+	public Date getRegDt() {
+		return regDt;
+	}
+	public void setRegDt(Date regDt) {
+		this.regDt = regDt;
+	}
+	public Integer getBoardViews() {
+		return boardViews;
+	}
+	public void setBoardViews(Integer boardViews) {
+		this.boardViews = boardViews;
+	}
+	public String getBoardContent() {
+		return boardContent;
+	}
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	@Override
+	public String toString() {
+		return "BoardVo [boardNo=" + boardNo + ", boardTitle=" + boardTitle + ", boardWriter=" + boardWriter
+				+ ", regDt=" + regDt + ", boardViews=" + boardViews + ", boardContent=" + boardContent + ", fileName="
+				+ fileName + "]";
+	}
+
+
+
+}
---
+++ MyPage/src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,25 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>메인 화면</title>
+<!--CSS-->
+<link href="<c:url value="/resources/css/style.css" />" rel="stylesheet">
+</head>
+<body>
+	<div class="right">
+		<a href="#">로그인</a> <a href="#">회원가입</a>
+	</div><br>
+	<div class="index">
+		<ul>
+			<li><a href="#">메인화면</a></li>
+			<li><a href="#">공지사항</a></li>
+			<li><a href="#">자유게시판</a></li>
+		</ul>
+	</div>
+
+</body>
+</html>
